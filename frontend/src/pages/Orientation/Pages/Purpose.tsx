b5df68249e18f6bcb714bd2c2f67c50e1e697c01
Nicholas Defranco <defrann8208@outlook.com>
2024-04-23 14:41:19 -0400

    Isolate checkbox list functionality for easy reuse

--- frontend/src/pages/Orientation/Pages/Purpose.tsx
+++ frontend/src/pages/Orientation/Pages/Purpose.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
-import onCheck, { fetchEnumerable } from "./utils";
-import { useRef } from 'react';
-import { capitalize } from "./utils";
+import React, { useState } from "react";
 import { gql } from "graphql-request";
 import './Purpose.css';
+import PurposeList from "../../../components/CheckboxList";
 
 const motivesQuery = gql`
     query GetMotives {
@@ -14,46 +12,17 @@ const motivesQuery = gql`
 `;
 
 function Purpose(props: any) {
-    const [options, setOptions] = useState<Array<string>>();
     const [motives, setMotives] = useState<Set<string>>(new Set(props.user.motives));
-    const input = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        fetchEnumerable(motivesQuery)
-            .then(setOptions)
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
-
-    if(options) {
-        return (
-            <div className="purpose-page">
-                <h3 className="main-heading">Let's get you stiched in</h3>
-                <p className="sub-heading">Find your team. What brings you to Regenquest?</p>
-                <p className="sub-text">Select all that apply</p>
-                
-                {options.map(
-                        (motive) => (
-                            <div className="select-container" key={motive}>
-                                <input
-                                    ref={input}
-                                    className="click-button"
-                                    onChange={(e) => onCheck([setMotives, props.updateData], motives, e)}
-                                    type="checkbox"
-                                    id={motive.toLowerCase()}
-                                    name={motive}
-                                    defaultChecked={motives.has(motive)} />
-    
-                                <label htmlFor={motive.toLocaleLowerCase()}>{capitalize(motive)}</label>
-                            </div>
-                        )
-                    )}
-            </div>
-        );
-    } else {
-        return null;
-    }
+    return (
+        <div className="purpose-page">
+            <h3 className="main-heading">Let's get you stiched in</h3>
+            <p className="sub-heading">Find your team. What brings you to Regenquest?</p>
+            <p className="sub-text">Select all that apply</p>
+            
+            <PurposeList setFuncs={[setMotives, props.updateData]} checkedData={motives} query={motivesQuery} />
+        </div>
+    );
 }
 
 export default Purpose;
